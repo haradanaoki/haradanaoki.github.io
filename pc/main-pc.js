d6587960adf833f40f3302bd9b9590ce2d135333
harada_naoki <harada_naoki@jp.fujitsu.com>
2016-09-01 14:59:27 +0900

    safda

---
+++ pc/main-pc.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+
+  var image = document.getElementById('image');
+
+  // app_idは自分のものに書き換えてください
+  var milkcocoa = new MilkCocoa("app_id.mlkcca.com");
+  var ds = milkcocoa.dataStore('gravity');
+
+  ds.on('send', changeViewFromSentMode);
+
+  function changeViewFromSentMode(sent){
+    if(sent.value.mode === 'portrait'){
+      image.className = '';
+    }
+    if(sent.value.mode === 'landscape'){
+      image.className = 'is-landscape';
+    }
+  }
+};
